c34cd9f655c9539051a8b0c42cf8305418287f80
Kevin Dunn <kjdunn1226@gmail.com>
2017-12-19 22:56:47 -0500

    fixed emails.py for Python 3

--- emails.py
+++ emails.py
@@ -1,5 +1,6 @@
 # First, run the command: heroku local:run -e dev.env python manage.py shell
 # Then, in the Django shell, run the command: execfile('emails.py')
+# exec(open('emails.py').read())
 
 from LCS.models import *
 import unicodedata
@@ -11,7 +12,7 @@ emails = []
 for tutor in tutors:
 	emails.append(unicodedata.normalize('NFKD', tutor.user.email).encode('ascii','ignore'))
 
-with open('emails.csv', 'wb') as csvfile:
+with open('emails.csv', 'wt') as csvfile:
     emailwriter = csv.writer(csvfile)
     for email in emails:
     	emailwriter.writerow([email])
